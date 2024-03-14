05add7288a171592673ec30882859e43563411a6
Snowbeeee <grivas.eduardo03@gmail.com>
2024-03-14 12:32:10 +0100

    test club deportivo alto rendimiento

--- src/test/java/clubdeportivo/ClubDeportivoAltoRendimientoTest.java
+++ src/test/java/clubdeportivo/ClubDeportivoAltoRendimientoTest.java
@@ -1,5 +1,19 @@
 package clubdeportivo;
 
+import org.junit.jupiter.api.*;
+
 public class ClubDeportivoAltoRendimientoTest {
-    
+    @BeforeEach
+
+
+    @Test
+    public void anyadirActividadCorrectLength() throws ClubException{
+        //Arrange
+        ClubDeportivoAltoRendimiento club = new ClubDeportivoAltoRendimiento("a", 5, 1);
+        int datos[] = {1,2,3,4};
+
+        //Act
+        //boolean resultado = club.
+
+    }
 }
