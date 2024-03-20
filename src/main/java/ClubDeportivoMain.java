74d028a3cd42010253e305fc1b5d4923d7f7dc1c
Snowbeeee <grivas.eduardo03@gmail.com>
2024-03-20 10:59:34 +0100

    .

--- src/main/java/ClubDeportivoMain.java
+++ src/main/java/ClubDeportivoMain.java
@@ -20,27 +20,5 @@ public class ClubDeportivoMain {
 			System.out.println(e.getMessage());
 		}
 
-		System.out.println("");
-
-		try {
-			ClubDeportivoAltoRendimiento club = new ClubDeportivoAltoRendimiento("UMA",1,1,1);
-			Grupo pilates = new Grupo("456B","Pilates",8,5,50.0);
-			club.anyadirActividad(grupo1);
-			club.anyadirActividad(pilates); // ???????
-			System.out.println(club);			
-			System.out.println("Ingresos: " + club.ingresos());
-			
-		} catch (ClubException e) {
-			System.out.println(e.getMessage());
-		}
-
-		/*ClubDeportivoAltoRendimiento club = new ClubDeportivoAltoRendimiento("nombre", 6, 1);
-        String datos[] = {"nombre", "actividad", "10", "7","30"};
-
-        club.anyadirActividad(datos);
-
-		System.out.println(club.toString());*/
-
-		
 	}
 }
